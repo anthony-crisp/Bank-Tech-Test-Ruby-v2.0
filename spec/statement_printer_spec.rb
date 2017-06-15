51b39922a93b9db759470e4e94a7905548726149
Anthony Crisp <anthony_crisp@hotmail.com>
2017-06-15 16:26:21 +0100

    formatting tweaks

--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -8,14 +8,13 @@ RSpec.describe PrintStatement do
     expect { subject.print_statement }.to output("date || credit || debit || balance\n").to_stdout
   end
 
-    it 'prints formatted statement to stdout' do
-      date = DateGenerator.new.todays_date
-      @transaction = [[date, 20, '', 20], [date, 10, '', 30]]
-      expect { PrintStatement.new(@transaction).print_statement }.to output(
-        "date || credit || debit || balance\n" \
-        "#{Time.now.strftime('%d/%m/%Y')} || 10 ||  || 30\n" \
-        "#{Time.now.strftime('%d/%m/%Y')} || 20 ||  || 20\n"
-      ).to_stdout
-    end
-
+  it 'prints formatted statement to stdout' do
+    date = DateGenerator.new.todays_date
+    @transaction = [[date, 20, '', 20], [date, 10, '', 30]]
+    expect { PrintStatement.new(@transaction).print_statement }.to output(
+      "date || credit || debit || balance\n" \
+      "#{Time.now.strftime('%d/%m/%Y')} || 10 ||  || 30\n" \
+      "#{Time.now.strftime('%d/%m/%Y')} || 20 ||  || 20\n"
+    ).to_stdout
+  end
 end
